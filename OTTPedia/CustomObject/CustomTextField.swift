ea37aa6ef6c069ae63cce8a757f0eafd82c09cf1
young <zerobugpark@gmail.com>
2025-01-24 20:38:05 +0900

    [파일 추가] - CustomTextField 파일 추가

---
+++ OTTPedia/CustomObject/CustomTextField.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTextField.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/24/25.
+//
+
+import UIKit
+
+final class CustomTextField: UITextField {
+    
+    
+    init(boldStyle: Bool, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = boldStyle ? .boldSystemFont(ofSize: fontSize) : .systemFont(ofSize: fontSize)
+    }
+    
+
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
