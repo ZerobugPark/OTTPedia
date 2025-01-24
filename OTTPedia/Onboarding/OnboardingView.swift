5c93c00a8d051d0a39c7dd9d4dadd4c48bc21968
young <zerobugpark@gmail.com>
2025-01-24 19:02:47 +0900

    [레이아웃]
    - Onboarding 레이아웃 구성

--- OTTPedia/Onboarding/OnboardingView.swift
+++ OTTPedia/Onboarding/OnboardingView.swift
@@ -7,14 +7,74 @@
 
 import UIKit
 
-class OnboardingView: UIView {
+import SnapKit
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+
+final class OnboardingView: BaseCollectionViewCell {
+
+    private let imageView = CustomImageView()
+    private let titleLable = CustomLabel(boldStyle: false, fontSize: 16, italic: true)
+    private let subTitleLable = CustomLabel(boldStyle: false, fontSize: 14, italic: false)
+    private let startButton = CustomButton(color: #colorLiteral(red: 0, green: 0.6, blue: 0.8392156863, alpha: 1))
+    
+    override func configureHierarchy() {
+        addSubview(imageView)
+        addSubview(titleLable)
+        addSubview(subTitleLable)
+        addSubview(startButton)
+        
     }
-    */
+    
+    override func configureLayout() {
+        
+        
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(self)
+            make.bottom.equalTo(titleLable.snp.top).inset(-4)
+        }
+        
+        titleLable.snp.makeConstraints { make in
+            make.bottom.equalTo(subTitleLable.snp.top).inset(-30)
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.height.equalTo(20)
+        }
+        
+        subTitleLable.snp.makeConstraints { make in
+            make.bottom.equalTo(startButton.snp.top).offset(-30)
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.height.equalTo(35)
+            
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.bottom.equalTo(self).inset(120)
+            //make.height.equalTo(50)
+            // 화면이 작아지면 작아질수록, 버튼의 사이즈는 작아지니까 고정 X
+            make.height.equalTo(self.snp.width).multipliedBy(1.0 / 8.0) // 뷰의 너비기준으로 높이 비율 계산 즉, 너비 * 비율
+            
+        }
 
+        
+    }
+    
+    override func configureView() {
+        
+        let title = "Onboarding"
+        let subtitle = "당신만의 영화 세상,\nOTTPedia를 시작해보세요."
+        
+        
+        imageView.image = UIImage(named: "onboarding")
+        
+        titleLable.text = title
+       
+        subTitleLable.text = subtitle
+        subTitleLable.numberOfLines = 2
+        
+        startButton.setTitle("시작하기", for: .normal)
+        
+        self.backgroundColor = .black
+        
+    }
 }
