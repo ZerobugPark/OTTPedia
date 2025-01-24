a8c890ca9e9485d7c5a907d3b0f4ee81b71d948f
young <zerobugpark@gmail.com>
2025-01-24 20:16:44 +0900

    [추가
    - colorList enum 구현

--- OTTPedia/Static/ColorList.swift
+++ OTTPedia/Static/ColorList.swift
@@ -5,4 +5,36 @@
 //  Created by youngkyun park on 1/24/25.
 //
 
-import Foundation
+import UIKit
+
+enum ColorList {
+    
+    case main
+    case black
+    case white
+    case lightGray
+    case DarkGray
+    
+    var color: UIColor {
+        switch self {
+        case .main:
+            return  #colorLiteral(red: 0, green: 0.6, blue: 0.8392156863, alpha: 1)
+        case .black:
+            return .black
+        case .white:
+            return .white
+        case .lightGray:
+            return  #colorLiteral(red: 0.8823529412, green: 0.8823529412, blue: 0.8823529412, alpha: 1)
+        case .DarkGray:
+            return  #colorLiteral(red: 0.5568627451, green: 0.5568627451, blue: 0.5568627451, alpha: 1)
+ 
+        }
+    
+    }
+    
+    //: UIColor = #colorLiteral(red: 0, green: 0.6, blue: 0.8392156863, alpha: 1)
+    
+    
+    
+    
+}
