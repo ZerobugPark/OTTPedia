c896f272f7bb06920d53a994aa2549b7436a8706
young <zerobugpark@gmail.com>
2025-01-25 11:06:46 +0900

    [파일 추가]
    - ProfileImageSettingCollectionViewCell

---
+++ OTTPedia/Profile/ProfileImageSettingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileImageSettingCollectionViewCell.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/25/25.
+//
+
+import UIKit
+
+class ProfileImageSettingCollectionViewCell: UICollectionViewCell {
+    
+}
