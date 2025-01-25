c5f023dda0cce0327ac9f80b9217bd3c94fb4b9d
young <zerobugpark@gmail.com>
2025-01-25 14:18:47 +0900

    [파일 추가] - 메인 뷰컨트롤러 - 셋팅 뷰컨트롤러 - 탭바 컨트롤러

---
+++ OTTPedia/Setting/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/25/25.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+}
---
+++ OTTPedia/TabBarController/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/25/25.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureTabBarController()
+    }
+    
+
+    private func configureTabBarController() {
+        let firstVc = 
+        
+        
+        
+        
+        
+        
+    }
+    
+
+}
