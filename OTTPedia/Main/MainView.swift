4862bb3737170b238367e735e6011883fd1f362c
young <zerobugpark@gmail.com>
2025-01-26 21:58:47 +0900

    [추가] - 레이아웃 추가 - addTarget Test

--- OTTPedia/Main/MainView.swift
+++ OTTPedia/Main/MainView.swift
@@ -9,20 +9,24 @@ import UIKit
 
 final class MainView: BaseView {
     
+    let view = CustomView()
     let imageView = CustomImageView(selected: true)
-    let nameLable = CustomLabel(boldStyle: true, fontSize: 16, italic: false)
+    let nameLable = CustomLabel(boldStyle: true, fontSize: 16, color:  ColorList.white.color)
     let likeStorageButton = CustomButton()
+    let tableView = UITableView()
+ 
     
-    private let view = CustomView()
-    private let dateLable = CustomLabel(boldStyle: false, fontSize: 12, italic: false)
-    private let chevronImage = CustomImageView() // //chevron.right
+    private let dateLable = CustomLabel(boldStyle: false, fontSize: 12, color: ColorList.lightGray.color)
+    private let chevronImage = CustomImageView()
     
     override func configureHierarchy() {
         addSubview(view)
         view.addSubview(imageView)
-        addSubview(nameLable)
-        addSubview(dateLable)
-        addSubview(chevronImage)
+        view.addSubview(nameLable)
+        view.addSubview(dateLable)
+        view.addSubview(chevronImage)
+        addSubview(likeStorageButton)
+        addSubview(tableView)
         
         
     }
@@ -30,17 +34,48 @@ final class MainView: BaseView {
     override func configureLayout() {
         
         view.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide)
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
             make.horizontalEdges.equalTo(self).inset(16)
-            make.height.equalTo(self.snp.width).multipliedBy(1.0 / 3.0)
+            make.height.equalTo(self.snp.width).multipliedBy(1.0 / 3.5)
         }
         
         imageView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
-            make.leading.equalTo(view.safeAreaLayoutGuide).offset(8)
-            make.size.equalTo(view.snp.width).multipliedBy(1.0 / 5.5)
+            make.top.equalTo(view).offset(8)
+            make.leading.equalTo(view).offset(8)
+            make.size.equalTo(view.snp.width).multipliedBy(1.0 / 6.5)
         }
         
+        nameLable.snp.makeConstraints { make in
+            make.centerY.equalTo(imageView).offset(-4)
+            make.leading.equalTo(imageView.snp.trailing).offset(8)
+            make.trailing.equalTo(view).inset(50)
+            make.height.equalTo(18)
+        }
+        
+        dateLable.snp.makeConstraints { make in
+            make.top.equalTo(nameLable.snp.bottom).offset(2)
+            make.leading.equalTo(imageView.snp.trailing).offset(8)
+            make.trailing.equalTo(view).inset(50)
+            make.height.equalTo(14)
+        }
+        
+        chevronImage.snp.makeConstraints { make in
+            make.centerY.equalTo(imageView)
+            make.trailing.equalTo(view).inset(16)
+            make.size.equalTo(imageView.snp.width).multipliedBy(1.0 / 2.5)
+        }
+        
+        likeStorageButton.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(view).inset(8)
+            make.bottom.equalTo(view).inset(8)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.bottom.equalTo(self)
+        }
     }
     
     override func configureView() {
@@ -48,24 +83,16 @@ final class MainView: BaseView {
         
         imageView.image = UIImage(named: ImageList.shared.profileImageList[0])
         
-        
         view.isUserInteractionEnabled = true // 뷰에도 터치 가능하게
-        //
-        //        circleView.backgroundColor = ColorList.main.color
-        //
-        //        subImageView.image = UIImage(systemName: "camera.fill")
-        //        subImageView.tintColor = .white
-        //        subImageView.contentMode = .scaleAspectFit
-        //
-        //        lineView.backgroundColor = ColorList.white.color
-        //
-        //        infoLable.textAlignment = .left
-        //        infoLable.textColor = ColorList.main.color
-        //        infoLable.text = "2글자 이상 10글자 미만으로 설정해주세요"
-        //
-        //        okButton.setTitle("완료", for: .normal)
-        //        okButton.isEnabled = false
+
+        nameLable.text = "안녕하세요안녕하세요"
+
+        dateLable.text = "25.01.23 가입"
+
+        chevronImage.image = UIImage(systemName: "chevron.compact.right")
+        chevronImage.tintColor = ColorList.lightGray.color
         
+        likeStorageButton.setTitle("18 개의 무비박스 보관중", for: .normal)
         
         DispatchQueue.main.async {
             self.imageView.layer.cornerRadius = self.imageView.frame.width / 2
--- OTTPedia/Main/MainViewController.swift
+++ OTTPedia/Main/MainViewController.swift
@@ -10,18 +10,67 @@ import UIKit
 final class MainViewController: UIViewController {
 
     
+    var list = ["test1", "test2"]
+    
     private var mainView = MainView()
     
     override func loadView() {
         view = mainView
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainView.tableView.rowHeight = 200
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
 
-       
+        configurationNavigationController()
+        mainView.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(profileButtonTapped)))
+        mainView.likeStorageButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
     }
     
 
-  
+    private func configurationNavigationController() {
+        
+        let title = "OTTPedia"
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationItem.title = title
+        navigationController?.navigationBar.tintColor = ColorList.main.color
+        UINavigationBar.appearance().standardAppearance = appearance
+        
+    }
 
+    @objc private func profileButtonTapped(_ sender: UIButton) {
+        
+        print(#function)
+    }
+    
+    @objc private func likeButtonTapped(_ sender: UIButton) {
+        
+        print(#function)
+    }
+    
+        
+
+}
+
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.titleLabel.text = list[indexPath.row]
+        
+        return UITableViewCell()
+    }
+    
+    
 }
