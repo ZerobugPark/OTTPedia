fe44088956e62c5b96c15a77d7752f4884bdd7b9
young <zerobugpark@gmail.com>
2025-01-27 16:36:33 +0900

    [이슈]
    - 서치 컨트롤러를 네비게이션바 영역에 등록했을 때, PhotoProject에서는 정상적으로 동작했었으나, 현재 서치뷰에서는 제대로 동작하지 않음
    - 발생원인은 뷰가 살아있는 상태에서 다른 탭뷰를 이동했다가 다시 돌아오면 서치뷰가 생겨있음.
    
    *확인 사항
    - 네비게이션 타이틀이 적용이 적용되는 것으로 보아서는 네비게이션이 닐인 것은 아님
    - WillAppear에서 서치바 컨트롤러를 적용했을 때도 동일한 현상 발생
    
    뇌피셜
    - PhotoProject에서는 탭바에에 연결된 네비게이션 뷰에 연결된 상태여서 문제가 없었으나, 지금은 탭바가 아닌 탭바 Right 버튼으로 이동하는 새로운 뷰가 만들어지기 때문에, 일정 이슈가 발생하는 것으로 보임.
    
    해결 방안
    - 서치바를 새로 만들어서 해결하는 것이 좋을 것으로 보임

--- OTTPedia/SearchView/SearchView.swift
+++ OTTPedia/SearchView/SearchView.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
-class SearchView: UIView {
+class SearchView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let searchController = UISearchController()
+    let tableView = UITableView()
+
+  
+    override func configureHierarchy() {
+        addSubview(tableView)
+
+        
     }
-    */
+    
+    override func configureLayout() {
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(60)
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+
+    }
+    
+    override func configureView() {
+        tableView.bounces = false
+        
+        //네비게이션 컨트롤러 영역 미침범
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.backgroundColor = ColorList.darkGray.color
+        
+        // Cancel 버튼 삭제
+        searchController.searchBar.setShowsCancelButton(false, animated: false)
+   
+    }
 
 }
---
+++ OTTPedia/SearchView/SearchViewController.swift
@@ -0,0 +1,100 @@
+//
+//  SearchViewController.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/27/25.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    
+    private var searchView = SearchView()
+    
+    override func loadView() {
+        view = searchView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurationNavigationController()
+        searchView.searchController.searchBar.delegate = self
+        view.backgroundColor = .blue
+    }
+    
+    private func configurationNavigationController() {
+        
+        let title = "영화검색"
+        navigationItem.title = title
+
+
+        searchView.searchController.searchBar.placeholder = "검색어 입력"
+    }
+
+  
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationItem.searchController = searchView.searchController
+    }
+
+}
+
+
+// MARK: - SearchBar Delegate
+extension SearchViewController: UISearchBarDelegate {
+   // func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+//        changeButtonColor(status: false)
+//        results.removeAll()
+//        filter =  "relevant"
+//        searchView.collectionView.reloadData()
+//        
+//        guard let text = searchBar.text?.replacingOccurrences(of: " ", with: ""), !text.isEmpty  else {
+//            let msg = "1글자 이상의 검색어를 입력해주세요."
+//            showAlert(msg: msg)
+//            // 서치바 원상태로 복귀
+//            searchView.searchController.isActive = false
+//            return
+//        }
+//        
+//        searchText = text
+//        page = 1
+//        
+//        NetworkManager.shared.callRequest(api: .searchImage(query: searchText, filter: filter, page: String(page)), type: UnslpashGetImage.self) { value in
+//           
+//            self.getInfo = value
+//             
+//            if self.getInfo.total == 0, self.getInfo.results.isEmpty {
+//                self.searchView.label.text = "검색결과가 없습니다."
+//                self.searchView.label.isHidden = false
+//            } else {
+//                self.searchView.label.isHidden = true
+//            }
+//            self.results = self.getInfo.results
+//            self.backupResults = self.results
+//            
+//            if self.getInfo.total > 0, !self.getInfo.results.isEmpty {
+//                self.searchView.collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+//            }
+//        } failHandler: { status in
+//            let msg = Error.errorMsg(satus: status)
+//            self.showAlert(msg: msg)
+//        }
+//        
+//    }
+//    
+//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+//        results.removeAll()
+//        searchView.collectionView.reloadData()
+//        self.searchView.label.text = "사진을 검색해보세요."
+//        self.searchView.label.isHidden = false
+//    }
+//    
+//    
+//    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+//        searchView.searchController.searchBar.text = ""
+//        return true
+//    }
+
+}
