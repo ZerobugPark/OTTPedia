92aa55853d17b274cd928717e702ee3f234d7481
young <zerobugpark@gmail.com>
2025-01-28 01:35:04 +0900

    [추가] - 페이지컨트롤러 및 스크롤 뷰 레이아웃 구현 및 백그라운드 이미지로 테스트

--- OTTPedia/DetailView/DetailView.swift
+++ OTTPedia/DetailView/DetailView.swift
@@ -7,14 +7,58 @@
 
 import UIKit
 
-class DetailView: UIView {
+import SnapKit
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class DetailView: BaseView {
+    
+    
+    let scrollView = UIScrollView()
+    let pageControl = UIPageControl()
+    let infoLabel = CustomLabel(boldStyle: false, fontSize: 12, italic: false, color: ColorList.lightGray.color)
+    
+    override func configureHierarchy() {
+        
+        addSubview(scrollView)
+        addSubview(pageControl)
+        addSubview(infoLabel)
+        
     }
-    */
+    
+    override func configureLayout() {
+        
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(self)
+            make.height.equalTo(self.snp.width).multipliedBy(1.0 / 1.75)
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.bottom.equalTo(self.scrollView.snp.bottom)
+            make.centerX.equalTo(scrollView)
+        }
+        
+        
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(scrollView.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(self).inset(16)
+            make.height.equalTo(15)
+        }
+  
+        
+    }
+    
+    override func configureView() {
+   
+       // scrollView.backgroundColor = .white
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        infoLabel.backgroundColor = .white
+        
+        pageControl.pageIndicatorTintColor = ColorList.lightGray.color
+        pageControl.currentPageIndicatorTintColor = ColorList.white.color
+        
+    }
+
+    
 
 }
--- OTTPedia/DetailView/DetailViewController.swift
+++ OTTPedia/DetailView/DetailViewController.swift
@@ -8,22 +8,67 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    
+    private var detailView = DetailView()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let color:[UIColor] = [.red,.blue,.green,.yellow,.purple]
+    override func loadView() {
+        view = detailView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        detailView.scrollView.delegate = self
+        
+        
+        
+        
+        
     }
-    */
+    
+    private func addContentScrollView() {
+        for i in 0..<5 {
+            let imageView = UIImageView()
+            let xPos = detailView.scrollView.frame.width * CGFloat(i)
+            // print(xPos)
+            imageView.frame = CGRect(x: xPos, y: 0, width: detailView.scrollView.bounds.width, height: detailView.scrollView.bounds.height)
+            imageView.backgroundColor = color[i]
+            detailView.scrollView.addSubview(imageView)
+            detailView.scrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
+        }
+    }
+    
+    private func setPageControl() {
+        detailView.pageControl.numberOfPages = 5
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        addContentScrollView()
+        setPageControl()
 
+    }
+    
+    
+    
 }
+
+extension DetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //scrollView.contentOffset.x : 스크롤뷰가 움직이는 실시간 좌표 값
+        let value = scrollView.contentOffset.x/scrollView.frame.size.width
+        // print(scrollView.contentOffset.x)
+        
+        //value 자체를 rounded를 할 경우, isNan 및 isInfinite 오류 발생
+        //해결 -> 타입 자체를 Int로 먼저 바꾸지 말고, 실수형으로 바꾼 뒤 Int로 변환해서 해결(정밀하지 않아도 되니 Float로 구현)
+        let index = lroundf(Float(value))
+        setPageControlSelectedPage(currentPage: Int(index))
+        
+    }
+    private func setPageControlSelectedPage(currentPage:Int) {
+        detailView.pageControl.currentPage = currentPage
+    }
+}
+
