ec7dfbc62f60a70aae2651496a65ac46aa80b805
young <zerobugpark@gmail.com>
2025-01-27 15:53:02 +0900

    [파일 추가] - 서치뷰 및 뷰컨트롤러 추가

---
+++ OTTPedia/SearchView/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/27/25.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
