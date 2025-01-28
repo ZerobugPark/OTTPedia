9b2eda600016297d8f6819a4b04b80aa4312cdff
young <zerobugpark@gmail.com>
2025-01-28 19:55:31 +0900

    [파일 추가] - CastCollectionViewCell 파일 추가

---
+++ OTTPedia/DetailView/Cast/CastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CastCollectionViewCell.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/28/25.
+//
+
+import UIKit
+
+class CastCollectionViewCell: BaseCollectionViewCell {
+    
+}
