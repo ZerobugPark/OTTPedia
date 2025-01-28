18f3cc1b5128467fe7487d04a5c4110d0724d010
young <zerobugpark@gmail.com>
2025-01-28 23:41:42 +0900

    [추가] - Credit API 구조체 추가

--- OTTPedia/TMDB/TMDBStruct.swift
+++ OTTPedia/TMDB/TMDBStruct.swift
@@ -18,10 +18,11 @@ struct Trending: Decodable {
 struct Results: Decodable {
     
     let backdropPath: String
+    let id: Int
     let title: String
     let overview: String
     let posterPath: String
-    let adult: Bool
+   // let adult: Bool
     let genreIds: [Int]
     let releaseDate: String
     let average: Double
@@ -29,10 +30,11 @@ struct Results: Decodable {
     
     enum CodingKeys: String, CodingKey {
         case backdropPath = "backdrop_path"
+        case id
         case title
         case overview
         case posterPath = "poster_path"
-        case adult
+       // case adult
         case genreIds = "genre_ids"
         case releaseDate = "release_date"
         case average = "vote_average"
@@ -41,10 +43,43 @@ struct Results: Decodable {
 }
 
 
+// MARK: - GetImage API
+struct GetImage: Decodable {
+    
+    let backdrops: [ImageInfo]
+    let posters: [ImageInfo]
+}
 
+struct ImageInfo: Decodable {
+    let height: Int
+    let filePath: String
+    let width: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case height
+        case filePath = "file_path"
+        case width
 
+        
+    }
+}
 
+// MARK: - Credit API
+struct GetCredit: Decodable {
+    
+    let cast: [CastInfo]
+}
 
+struct CastInfo: Decodable {
+    let name: String
+    let profilePath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case profilePath = "profile_path"
+    
+    }
+}
 
 
 
