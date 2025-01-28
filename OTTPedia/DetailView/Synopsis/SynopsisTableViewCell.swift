d65897d70db1511ac1a3686296574b06a78e571d
young <zerobugpark@gmail.com>
2025-01-28 16:38:43 +0900

    [파일추가] - SynopsisTableViewCell 파일 추가

---
+++ OTTPedia/DetailView/Synopsis/SynopsisTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SynopsisTableViewCell.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/28/25.
+//
+
+import UIKit
+
+class SynopsisTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
