b26420a1261a574ce79571c53f65d3e24c9bb7bb
young <zerobugpark@gmail.com>
2025-01-29 10:21:19 +0900

    [파일 추가] - Modify 뷰컨트롤러 추가

---
+++ OTTPedia/ModifyProfile/ModifiyProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ModifiyProfileViewController.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/29/25.
+//
+
+import UIKit
+
+class ModifiyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
