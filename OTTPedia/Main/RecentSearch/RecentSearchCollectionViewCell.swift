d2a2d0b3a87710603aee5988da52adac59574680
young <zerobugpark@gmail.com>
2025-01-30 22:26:34 +0900

    [파일추가]
    - RecentSearchCollectionViewCell

---
+++ OTTPedia/Main/RecentSearch/RecentSearchCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecentSearchCollectionViewCell.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/30/25.
+//
+
+import UIKit
+
+class RecentSearchCollectionViewCell: UICollectionViewCell {
+    
+}
