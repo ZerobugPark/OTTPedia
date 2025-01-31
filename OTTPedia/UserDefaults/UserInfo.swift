7464c37bb828ca1891b7eeda36947f7c648ce5e5
young <zerobugpark@gmail.com>
2025-01-31 16:14:22 +0900

    [파일 추가]
    User Info 구조체 추가

---
+++ OTTPedia/UserDefaults/UserInfo.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfo.swift
+//  OTTPedia
+//
+//  Created by youngkyun park on 1/31/25.
+//
+
+import Foundation
+
+
+struct UserInfo {
+    let userImageIndex: Int
+    let id: String
+    let date: String
+    let likeCount: Int
+    
+}
