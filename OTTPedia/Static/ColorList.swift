6f8d88844a55c5ff6e100823ca995ad1e5ee9cdc
young <zerobugpark@gmail.com>
2025-02-11 22:01:56 +0900

    [이름수정]
    시스템 컬러와 동일한 이름으로 사용되기 때문에 네이밍 변경

--- OTTPedia/Static/ColorList.swift
+++ OTTPedia/Static/ColorList.swift
@@ -10,8 +10,8 @@ import UIKit
 enum ColorList {
 
     static let main = UIColor(resource: .mainBlue)
-    static let lightGray = UIColor(resource: .lightGray)
-    static let darkGray = UIColor(resource: .darkGray)
+    static let lightGray = UIColor(resource: .myLightGray)
+    static let darkGray = UIColor(resource: .myDarkGray)
     static let black = UIColor.black
     static let white = UIColor.white
     static let red = UIColor.red
